85c2c4b84ccd116fc57a4261cb95cba25fa3ff31
unknown <michaelzeakis13@gmail.com>
2025-08-09 16:10:31 +0300

    added main

---
+++ main.py
@@ -0,0 +1,32 @@
+#import libraries 
+from data import DMU_group_dataset
+
+
+if __name__ == "__main__":
+    
+    #data configuration 
+    n_dmus = 10
+    n_inputs = 5
+    n_outputs = 3
+
+    input_range = [1,10]
+    output_range = [5,40]
+    
+    
+    my_dataset = DMU_group_dataset(n_dmus, n_inputs, n_outputs, input_range, output_range)
+    
+    #generate data 
+    my_dataset.data_generator()    
+    
+    print("e?")
+    #calculate efficiencies 
+    
+    #manipulate data to the correct form 
+    
+    #train-test split 
+    
+    #load model 
+    
+    #feed model (train data)
+     
+    #evaluate model (test data)   
