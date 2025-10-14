d9ab5f89ae9ee0c88a0c73218acdca4008388f4d
Michael Zeakis <mzeakis@mobics.gr>
2025-10-14 21:15:24 +0300

    configuration fixed, removed single DMU group creation, added train-test split

--- main.py
+++ main.py
@@ -1,41 +1,71 @@
 #import libraries 
 from data.group_dataset import DMU_group
+from data.compined_dataset import Group_collection
+from data.visualization import plot_efficiencies
 from efficiency_scores import dea_input_oriented_extended
+
+from torch.utils.data import random_split, DataLoader
+import os
+
 
 
 if __name__ == "__main__":
+    #you might need this line for an error!
+    #os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE" 
     
-    #data configuration for 1 group 
-    #in training we will need N groups 
-    
+    #every group will have    
     n_dmus = 10
-    n_inputs = 5
-    n_outputs = 3
-
+    n_inputs = 2
+    n_outputs = 2
+    
+    
     input_range = [1,100]
     output_range = [1,400]
+    
+    n_groups = 6
+
     
     
     my_dataset = DMU_group(n_dmus, n_inputs, n_outputs, input_range, output_range)
     
-    #generate data 
-    my_dataset.data_generator()    
+    #dataset creation
+    dataset = Group_collection()
+    for group_index in range(n_groups):
+        current_dataset = DMU_group(n_dmus, n_inputs, n_outputs, input_range, output_range)
+        current_dataset.data_generator()
+        
+        #calculate efficiencies
+        for dmu_index in range(n_dmus):
+            theta, s_minus, s_plus, lambdas = dea_input_oriented_extended(current_dataset.inputs, current_dataset.outputs, dmu_index)
+            current_dataset.efficiencies.append(round(theta.item(),6))
+            
+            
+        dataset.add_group(current_dataset)
     
-    X = my_dataset.inputs
-    Y = my_dataset.outputs
     
-    #calculate efficiencies
-    for dmu_index in range(n_dmus):
-        theta, s_minus, s_plus, lambdas = dea_input_oriented_extended(X, Y, dmu_index)
-        my_dataset.efficinces.append(round(theta.item(),6))
-    print(f"These are the efficincy scores for one groupf of {n_dmus} DMUs: \n {my_dataset.efficinces}")
+    
+    
+    
+    #plot_efficiencies(my_dataset.efficiencies)
     
     #manipulate data to the correct form 
+    dataset.data_manipulation()
     
-    #train-test split 
+    #train-test split
+    train_size = int(0.8*len(dataset))
+    test_size = int(len(dataset)-train_size)
+    
+    train_dataset, test_dataset = random_split(dataset,[train_size,test_size])
+    
+    #dataloaders
+    train_loader = DataLoader(train_dataset,batch_size=4)
+    test_loader = DataLoader(test_dataset,batch_size=2)
+
     
     #load model 
     
+    
     #feed model (train data)
+    
      
     #evaluate model (test data)   
