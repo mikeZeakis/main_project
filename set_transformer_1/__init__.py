ae12390e652dd09a18d1516b359da8c16dc041d7
MikeZe <michaelzeakis13@gmail.com>
2026-05-02 15:28:08 +0300

    official pytorch implementation of set trasnformer - 1st approach

---
+++ set_transformer_1/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  1 18:38:50 2026
+
+@author: michalis
+"""
+
---
+++ set_transformer_1/model.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  1 18:36:04 2026
+
+@author: michalis
+"""
+
+from .modules import ISAB, PMA, SAB
+import torch.nn as nn
+
+
+class SetTransformer(nn.Module):
+    def __init__(self, dim_input, num_outputs, dim_output,
+            num_inds=32, dim_hidden=128, num_heads=4, ln=False):
+        super(SetTransformer, self).__init__()
+        self.enc = nn.Sequential(
+                ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln),
+                ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln))
+        self.dec = nn.Sequential(
+                PMA(dim_hidden, num_heads, num_outputs, ln=ln),
+                SAB(dim_hidden, dim_hidden, num_heads, ln=ln),
+                SAB(dim_hidden, dim_hidden, num_heads, ln=ln),
+                nn.Linear(dim_hidden, dim_output))
+
+    def forward(self, X):
+        return self.dec(self.enc(X))
